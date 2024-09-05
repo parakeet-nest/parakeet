bc85ef63f4ef7e5a59fd6ba82d5fc099bcdd7eca
@k33g <ph.charriere@gmail.com>
2024-09-06 00:59:28 +0200

    🛟 Updated: gitlab pipeline sample

--- examples/48-testing-models/04-yi-coder-gitlab-ci/main.go
+++ examples/48-testing-models/04-yi-coder-gitlab-ci/main.go
@@ -17,8 +17,8 @@ https://github.com/01-ai/Yi-Coder/blob/main/cookbook/System_prompt/System_prompt
 
 func main() {
 	ollamaUrl := "http://localhost:11434"
-	//model := "yi-coder:9b"
-	model := "yi-coder:1.5b"
+	model := "yi-coder:9b"
+	//model := "yi-coder:1.5b"
 
 	systemContent := `SYSTEM:
 	You are Yi-Coder, you are exceptionally skilled in programming, coding, devops and any computer-related issues.
@@ -34,17 +34,30 @@ func main() {
 		codebase += "<>\n```yaml\n" + pipeline + "```\n<>\n"
 	}
 
+	//userContent := `[Step-by-Step] Using the above codebase, explain the GitLab CI pipeline.
+	//Make a detqiled section per CI job.`
 
-	userContent := `[Step-by-Step] Using the above codebase, explain the GitLab CI pipeline.
-	Make a detqiled section per CI job.`
+	userContent := `Using the above the GitLab CI pipeline configuration in YAML format. 
+	Please analyze this pipeline step by step and explain the following details:
+
+      1. **Overview of the Pipeline**: Provide a high-level summary of the purpose and structure of the pipeline.
+      2. **Pipeline Stages**: List and describe each stage defined in the pipeline, explaining their purpose and order of execution.
+      3. **Jobs within Stages**: For each stage, detail the jobs that are included, explaining the commands or scripts they execute, and how they contribute to the overall pipeline.
+      4. **Triggers and Conditions**: Identify any triggers, conditions, or rules that dictate when the pipeline runs or when specific jobs are executed.
+      5. **Artifacts and Dependencies**: Explain how artifacts are used or shared between jobs, and describe any dependencies between jobs or stages.
+      6. **Environment Configuration**: Detail any environment-specific configurations, variables, or secrets used within the pipeline.
+      7. **Error Handling and Notifications**: Describe how the pipeline handles errors and if there are any notifications set up for failed jobs or stages.
+      8. **Optimization or Improvements**: Suggest potential optimizations or improvements to enhance the efficiency or reliability of the pipeline.
+
+    Please provide the explanation in a clear, concise manner, with examples where applicable.`
 
 	options := llm.Options{
 		Temperature: 0.0,
 	}
 
 	query := llm.GenQuery{
-		Model: model,
-		Prompt: systemContent + codebase + userContent,
+		Model:   model,
+		Prompt:  systemContent + codebase + userContent,
 		Options: options,
 	}
 
