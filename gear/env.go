ef44d0bd8a61277f69df38b0430db39ce45c1382
@k33g <ph.charriere@gmail.com>
2025-02-17 08:22:08 +0100

    🎨 Improve structure / format of the code.

--- gear/env.go
+++ gear/env.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-
+/*
 func GetEnvFloat(key string, defaultValue float64) float64 {
 	envValue := os.Getenv(key)
 	if envValue == "" {
@@ -17,7 +17,16 @@ func GetEnvFloat(key string, defaultValue float64) float64 {
 	}
 	return value
 }
+*/
 
+func GetEnvFloat(key string, defaultValue float64) float64 {
+	if value, err := strconv.ParseFloat(os.Getenv(key), 64); err == nil {
+		return value
+	}
+	return defaultValue
+}
+
+/*
 func GetEnvInt(key string, defaultValue int) int {
 	envValue := os.Getenv(key)
 	if envValue == "" {
@@ -29,6 +38,15 @@ func GetEnvInt(key string, defaultValue int) int {
 	}
 	return value
 }
+*/
+
+func GetEnvInt(key string, defaultValue int) int {
+	if value, err := strconv.Atoi(os.Getenv(key)); err == nil {
+		return value
+	}
+	return defaultValue
+}
+
 
 func GetEnvString(key string, defaultValue string) string {
 	envValue := os.Getenv(key)
